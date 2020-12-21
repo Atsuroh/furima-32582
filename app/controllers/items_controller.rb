1a937c7a4e9c67bfe8de084e83e67e394133fd09
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2020-12-21 21:46:39 +0900

    Category_id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,23 @@
 class ItemsController < ApplicationController
   def index
-    #@items = Item.all
+    @items = Item.all
   end
 
   def new
+    @items = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product_name, :text, :category_id, :status_id, :burden_id, :prefecture_id, :price)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :user
+
+  validates :product_name, :text, :price, presence: true
+  validates :category_id, numericality: { other_than: 1 }
 end
