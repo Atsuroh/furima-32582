4de6e29242adbe16d3da7ef91bfc656541caa1df
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2020-12-24 16:59:01 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update, :forbit_correct_user]
+  before_action :forbit_correct_user, only: [:edit, :update] 
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
@@ -19,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
         redirect_to item_path(@item)
     else
@@ -42,5 +41,15 @@ end
                                 :status_id, :burden_id,
                                 :prefecture_id, :shipping_id,
                                 :price).merge(user_id: current_user.id)
+  end
+
+  def forbit_correct_user
+    if @item.user.id != current_user.id
+      redirect_to root_path
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
