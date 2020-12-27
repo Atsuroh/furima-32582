85d62bd68780536188d4903a507b4d0c19ae0232
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2020-12-27 13:12:06 +0900

    購入機能実装

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,26 +1,31 @@
 const pay = () => {
-  Payjp.setPublicKey(""); // PAY.JPテスト公開鍵
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const form = document.getElementById("charge-form");
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    const formData = new FormData(form);
+    
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
     const card = {
-      number: formData.get("delivery[number]"),
-      cvc: formData.get("delivery[cvc]"),
-      exp_month: formData.get("delivery[exp_month]"),
-      exp_year: `20${formData.get("delivery[exp_year]")}`,
+      number: formData.get("order[number]"),
+      exp_month: formData.get("order[exp_month]"),
+      exp_year: `20${formData.get("order[exp_year]")}`,
+      cvc: formData.get("order[cvc]"),
     };
+
     Payjp.createToken(card, (status, response) => {
       if (status == 200) {
         const token = response.id;
         const renderDom = document.getElementById("charge-form");
-        const tokenObj = `<input value=${token} name='token' type="hidden"> `;
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
         renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
-      document.getElementById("card_number").removeAttribute("name");
-      document.getElementById("card_cvc").removeAttribute("name");
-      document.getElementById("card_exp_month").removeAttribute("name");
-      document.getElementById("card_exp_year").removeAttribute("name");
+
+      document.getElementById("card-number").removeAttribute("name");
+      document.getElementById("card-exp-month").removeAttribute("name");
+      document.getElementById("card-exp-year").removeAttribute("name");
+      document.getElementById("card-cvc").removeAttribute("name");
 
       document.getElementById("charge-form").submit();
     });
