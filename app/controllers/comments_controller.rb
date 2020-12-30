91adc782ff01255d99bcb7db502bd4700923f7af
Atsuroh <o89.yamanouchi.atsuro@gmail.com>
2020-12-30 22:38:15 +0900

    コメント途中

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to item_comments_path(@comment.item)
+    else
+      @item = @comment.item
+      @comments = @item.comments
+      render "items/show"
+    end
+  end
+
+  def destroy
+    comment = Comment.find_by(id: params[:id], item_id: params[:item_id])
+    if comment.user.id != current_user.id
+      redirect_to item_path(comment.item)
+    end
+    comment.destroy
+    redirect_to item_path(comment.item)
+  end
+
+  private
+  def comment_params
+    params.permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
-  resources :items, except: :index do
+  resources :users, only: :show
+  resources :items do
     resources :purchases, only: [:index, :create]
+    resources :comments, only: [:create, :destroy]
   end
 end
